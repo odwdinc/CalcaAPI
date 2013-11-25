339038c245a4474aedcffc2f9146337ee88e821a
odwdinc <anthony.pray@gmail.com>
2013-11-25 00:21:32 -0800

    Moved text to HTMLdata var

--- Calca.node.js
+++ Calca.node.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var request = require('request');
-
+var HTMLData = require('./data');
 var url = require('url');
 
 
@@ -14,11 +14,7 @@ function run_cmd(cmd, args, callBack ) {
 }
 
 
-function hereDoc(f) {
-  return f.toString().
-      replace(/^[^\/]+\/\*!?/, '').
-      replace(/\*\/[^\/]+$/, '');
-}
+
 
 
 var server = http.createServer(function (req, resp) {
@@ -49,78 +45,12 @@ var server = http.createServer(function (req, resp) {
               resp.end();
             });
         }else{
-          resp.end(hereDoc(function() {
-            /*!  
-              Please Use POST;
-              Format data:
-                m:('r'|'w'),        ;'r' for read curent text, 'w' overites with data string 
-                st:'data string'    ; not used for read, set cutent test in Calca
-              Allwas returns Calca's curent results. 
-           */
-         }));
-
+          resp.end(HTMLData.hereDoc(HTMLData.POST));
         }
-
       });
       
     }else{
-      resp.end(hereDoc(function() {
-      /*!  
-      <!DOCTYPE html> 
-        <html>  
-          <head>
-            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
-            </script>
-            <script>
-              function syinc(mode){
-                  var text = $("#tx1").val(); 
-                  $.post("CalcaAPI.html",{
-                        m:mode,
-                        st:text.replace(/\n/g, "<br/>").replace(/ /g, "&nbsp")
-                    },
-                      function(data,status){
-                        $("#tx1").val(data);
-                      });
-              };
-
-                $(document).ready(function(){
-                  $("textarea").keydown(function(e) {
-                      if(e.keyCode === 9) { // tab was pressed
-                          // get caret position/selection
-                          var start = this.selectionStart;
-                          var end = this.selectionEnd;
-
-                          var $this = $(this);
-                          var value = $this.val();
-
-                          // set textarea value to: text before caret + tab + text after caret
-                          $this.val(value.substring(0, start)
-                                    + "\t"
-                                    + value.substring(end));
-
-                          // put caret at right position again (add one for the tab)
-                          this.selectionStart = this.selectionEnd = start + 1;
-
-                          // prevent the focus lose
-                          e.preventDefault();
-                      }
-                  })
-               
-                  $("button").click(function(){
-                      syinc("w");
-                  });
-
-                  syinc("r");
-                });
-            </script>
-          </head>
-
-          <body> 
-            <button>Syinc</button></br>
-            <textarea id="tx1" rows="50" cols="200"></textarea> 
-          </body>
-        </html>
-      */}));             
+      resp.end(HTMLData.hereDoc(HTMLData.index));             
     }
 })
 
